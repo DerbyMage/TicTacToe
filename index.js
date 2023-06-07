332fea720d3e41614f14c9c899a67c309008eb30
DerbyMage <133152112+DerbyMage@users.noreply.github.com>
2023-06-07 17:22:56 -0400

    updating the gameboard with event handlers

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 //TicTacToe board and game
+const playerFactory = (name, letter) => {
+    let turn; 
+    return { name, letter, turn}
+
+}
 
 function playGame() {
-const rowOne = document.querySelector('div.row1>.square1')
-
-rowOne.innerText = 'B'
+    const newName = prompt('What is your name?')
+    const boardArr = []
+    const rowOne = document.querySelectorAll('div.row1>.square')
+    newName
+    const playerOne = playerFactory(newName, 'X')
+    playerOne.turn = true
+    console.log(playerOne)
+    const playerTwo =playerFactory('computer', 'O')
+    playerTwo.turn = false
+    console.log(playerTwo)
+    rowOne.addEventListener('click', function(e) {
+        console.log(e.target)
+    })
 
 }
