511659ff9caed0119f00ebc766e61314ce492d57
DerbyMage <133152112+DerbyMage@users.noreply.github.com>
2023-06-13 23:36:32 -0400

    minor updates

--- index.js
+++ index.js
@@ -30,4 +30,5 @@ function playGame() {
 
 const gameBoard = {
  boardArr: [{position: 1}, {position: 2}, {position: 3}, {position: 4}, {position: 5}, {position: 6}, {position: 7}, {position: 8}, {position: 9}]
+ 
 }
