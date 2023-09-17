d18370892180b5fc1d88291c81d71bd2e5bb638b
DerbyMage <133152112+DerbyMage@users.noreply.github.com>
2023-09-17 02:51:46 -0400

    updated the UI to look like an actual tic tac toe board

--- index.js
+++ index.js
@@ -9,24 +9,34 @@ function playGame() {
     const newName = prompt('What is your name?')
     const rowOne = document.querySelectorAll('.square')
     const playerOne = playerFactory(newName, 'X')
+    const gameArray = document.querySelectorAll('div[data-pos]')
+    console.log(gameArray[0])
+    console.log(gameArray[0].dataset.pos)
     playerOne.turn = true
     console.log(playerOne)
-    const playerTwo =playerFactory('computer', 'O')
+    const playerTwo = playerFactory('computer', 'O')
     playerTwo.turn = false
     console.log(playerTwo)
     console.log(rowOne)
     rowOne.forEach(ele => {ele.addEventListener('click', function(e) {
         console.log(e.target)
         
-       if(ele.innerText === '' ) { ele.innerText = playerOne.turn === true ? playerOne.letter: playerTwo.letter
+       if(ele.innerText === '' ) {
+        ele.innerText = playerOne.turn === true ? playerOne.letter: playerTwo.letter
         playerOne.turn = !playerOne.turn
-        playerTwo.turn = !playerTwo.turn} 
+        playerTwo.turn = !playerTwo.turn 
+        if(e.target.dataset.pos === '1') {
+            e.target.style.backgroundColor = 'green'
+            }
+        }
         else { alert('Space has already been taken') }
 
-       
-    })})
-
+      
+       })})
+      
 }
+
+
 
 const gameBoard = {
  boardArr: [{position: 1}, {position: 2}, {position: 3}, {position: 4}, {position: 5}, {position: 6}, {position: 7}, {position: 8}, {position: 9}]
